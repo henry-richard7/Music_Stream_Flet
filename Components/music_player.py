1bc32525dec8c830e3616070951e97169780134c
Henry-Richard7 <henry.richard.2k@gmail.com>
2024-06-14 07:31:30 +0530

    Created skeleton for Audio Player

---
+++ Components/music_player.py
@@ -0,0 +1,47 @@
+from flet import (
+    Text,
+    Audio,
+    Image,
+    ElevatedButton,
+    Row,
+    ControlEvent,
+    Slider,
+    IconButton,
+    Card,
+    icons,
+    ScrollMode,
+    Column,
+    ProgressBar,
+    AlertDialog,
+    alignment,
+)
+
+from API import YoutubeMusicApi
+
+
+class MusicPlayer(Column):
+    def __init__(
+        self, video_id: str, song_name: str, artist_name: str, album_name: str, art: str
+    ):
+        super().__init__()
+        self.video_id = video_id
+        self.song_name = song_name
+        self.artist_name = artist_name
+        self.album_name = album_name
+        self.art = art
+        self.padding = 10
+
+        yt_api = YoutubeMusicApi()
+        parsed_lyrics = yt_api.fetch_lyrics(video_id)
+
+        if parsed_lyrics["success"]:
+            lyrics = parsed_lyrics["results"]
+        else:
+            lyrics = "Lyrics Not Available!"
+
+        music_result = yt_api.get_direct_link(video_id)
+        direct_link = music_result["results"]["url"]
+        duration = music_result["results"]["approxDurationMs"]
+
+        self.player_state_ = "playing"
+        self.duration = duration
